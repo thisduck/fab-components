c335042a7289a78bb7b0fb3d37a7029e9848a711
Adnan Ali <adnan.ali@gmail.com>
2022-06-16 20:17:31 -0400

    Fix button with on isRunning.

--- frontend/src/components/RandomButton.vue
+++ frontend/src/components/RandomButton.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { inject, defineProps, computed, Ref } from 'vue';
+import { inject, defineProps, computed, Ref, ref, watch } from 'vue';
 
 const props = defineProps<{
   type?: 'submit' | 'button' | 'reset';
@@ -21,26 +21,43 @@ const isRunning = computed(() => {
   return isSubmitType.value && formIsRunning?.value;
 });
 
+const width = ref(0);
+const buttonRef = ref();
+
+watch(isRunning, (newValue, oldValue) => {
+  console.log(buttonRef);
+  if (!oldValue && newValue) {
+    width.value = buttonRef.value.offsetWidth;
+  } else {
+    width.value = 0;
+  }
+  console.log(newValue, oldValue);
+});
+
 const size = computed(() => {
   return props.size || 'base';
 });
 
 const buttonClasses = computed(() => {
+  const classes: string[] = [];
   const buttonSizes = {
     xs: 'py-1 px-2 text-xs',
     sm: 'py-1.5 px-2.5 text-sm',
     base: 'py-2 px-3 text-base',
   };
-  return [buttonSizes[size.value]];
+  classes.push(buttonSizes[size.value]);
+  return classes;
 });
 </script>
 
 <template>
   <button
+    ref="buttonRef"
     :disabled="disabledValue"
     :type="props.type"
     class="inline-flex items-center justify-center space-x-2 rounded-md bg-primary-700 text-gray-100 transition-all shadow-md hover:bg-primary-600 focus:ring-primary-400 focus:ring-offset-2 focus:ring-2"
     :class="[{ running: isRunning }, buttonClasses]"
+    :style="width ? `width: ${width}px` : ''"
   >
     <span v-if="isRunning" class="inline-block">
       <GgSpinner class="inline-block h-6 w-6 animate-spin text-gray-100" />
