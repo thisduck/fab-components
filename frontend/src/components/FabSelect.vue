d1c1708721f0158ee0cd266239372df0e35a4423
Adnan Ali <adnan.ali@gmail.com>
2022-07-04 20:50:52 -0400

    Converting FabSelect.

--- frontend/src/components/FabSelect.vue
+++ frontend/src/components/FabSelect.vue
@@ -20,13 +20,24 @@ const optionsValue = computed(() => {
 </script>
 
 <template>
-  <FabField as="select" class="py-2 px-3 rounded-md border border-gray-600 w-full">
-    <option v-if="placeholder" value="">
-      {{ placeholder }}
-    </option>
-    <option v-for="[value, label] of Object.entries(optionsValue)" :key="value" :value="value">
-      {{ label }}
-    </option>
+  <FabField>
+    <template #default="{ field }">
+      <select
+        :name="field.name"
+        :id="field.name"
+        v-model="field.value.value"
+        @input="field.handleChange"
+        @blur="field.handleBlur"
+        class="py-2 px-3 rounded-md border border-gray-600 w-full"
+      >
+        <option v-if="placeholder" value="">
+          {{ placeholder }}
+        </option>
+        <option v-for="[value, label] of Object.entries(optionsValue)" :key="value" :value="value">
+          {{ label }}
+        </option>
+      </select>
+    </template>
     <template v-if="$slots.label" #label>
       <slot name="label"></slot>
     </template>
