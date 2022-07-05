c10ca6f5282c7d4a03b5c787ea8accf4f4f3b6bf
Adnan Ali <adnan.ali@gmail.com>
2022-07-04 20:47:41 -0400

    Setting base for useField. Converting FabInput.

--- frontend/src/components/FabField.vue
+++ frontend/src/components/FabField.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
-import { ErrorMessage, Field } from 'vee-validate';
-import { computed, useAttrs } from 'vue';
+import { ErrorMessage, useField } from 'vee-validate';
+import { computed } from 'vue';
 import startCase from 'lodash/startCase';
 
 const props = defineProps<{
@@ -12,11 +12,11 @@ const props = defineProps<{
   onLabelClick?: () => void;
 }>();
 
+const fieldParams = useField(props.name);
+
 const labelValue = computed(() => {
   return props.label ?? startCase(props.name.split('.').reverse()[0]);
 });
-
-const attrs = useAttrs();
 </script>
 
 <script lang="ts">
@@ -32,17 +32,7 @@ export default {
       <span v-else>{{ labelValue }}</span>
     </FabLabel>
     <div :class="inputWrapperClasses">
-      <Field
-        :id="name"
-        :name="name"
-        :validateOnBlur="false"
-        :validateOnChange="false"
-        v-bind="attrs"
-      >
-        <template v-if="$slots.default" #default="fieldParams">
-          <slot name="default" v-bind="fieldParams"></slot>
-        </template>
-      </Field>
+      <slot name="default" v-bind="{ field: fieldParams }"></slot>
       <FabLabel v-if="props.label !== 'false' && labelAfter" :for="name" @click="onLabelClick">
         <slot v-if="$slots.label" name="label"></slot>
         <span v-else>
--- frontend/src/components/FabInput.vue
+++ frontend/src/components/FabInput.vue
@@ -1,16 +1,25 @@
 <script setup lang="ts">
 defineProps<{
+  name: string;
   type?: string;
   placeholder?: string;
 }>();
 </script>
 
 <template>
-  <FabField
-    :placeholder="placeholder"
-    :type="type || 'text'"
-    class="py-2 px-3 rounded-md border border-gray-600 w-full"
-  >
+  <FabField :name="name">
+    <template #default="{ field }">
+      <input
+        :name="field.name"
+        :id="field.name"
+        v-model="field.value.value"
+        @input="field.handleChange"
+        @blur="field.handleBlur"
+        :placeholder="placeholder"
+        :type="type || 'text'"
+        class="py-2 px-3 rounded-md border border-gray-600 w-full"
+      />
+    </template>
     <template v-if="$slots.label" #label>
       <slot name="label"></slot>
     </template>
