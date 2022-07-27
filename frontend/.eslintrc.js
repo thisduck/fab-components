b0bc1eac3cf6a30e91c9d631cf9e7a280fc0b9a3
Adnan Ali <adnan.ali@gmail.com>
2022-07-26 20:07:48 -0400

    Fixing up eslint config.

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -1,11 +1,19 @@
 module.exports = {
   env: {
+    browser: true,
+    es2021: true,
     node: true,
   },
+  parser: 'vue-eslint-parser',
+  parserOptions: {
+    parser: '@typescript-eslint/parser',
+    ecmaVersion: 2021,
+  },
+  plugins: ['@typescript-eslint'],
   extends: [
     'eslint:recommended',
     'plugin:vue/vue3-recommended',
-    'plugin:tailwindcss/recommended',
+    'plugin:@typescript-eslint/recommended',
     'prettier',
   ],
   rules: {
